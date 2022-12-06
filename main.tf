160980c9d836079b88d3a101ff38a577040e048b
sanu1020 <sanuuthaya02@gmail.com>
2022-12-06 10:54:57 +0530

    added workflow

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "cicd-githubactions" {
+  ami           = "ami-005e54dee72cc1d00" 
+  instance_type = "t2.micro"
+  key_name = "terraform-key"
+
+  tags = {
+    Name = "cicd pipeline github actions"
+  }
+ 
+}
